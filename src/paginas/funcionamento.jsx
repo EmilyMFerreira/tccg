52f627af91f28879b1aaf6b19b99b5f44f98e911
EmilyMFerreira <marcelineferrera@gmail.com>
2023-11-16 18:08:30 -0300

    img funcionamento

--- src/paginas/funcionamento.jsx
+++ src/paginas/funcionamento.jsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom'
 import './css/funcio.css'
 
+import funcionamento from "../imagens/funcionamento1-11.png"
+
 function Funcionamento() {
     return (
     <>
@@ -10,7 +12,7 @@ function Funcionamento() {
     </header>
 
         <div className="func1">
-        <img src="src/imagens/funcionamento1-11.png" alt="funcio1" />
+        <img src={funcionamento} alt="funcio1" />
       <p>
     O Arduino é uma plataforma amplamente utilizada na prototipagem eletrônica graças à sua facilidade de programação e implantação. Hoje faremos uso dessas características para 
     desenvolver um robô seguidor de linha e entender na prática como essas máquinas amplamente utilizadas na indústria funcionam.
