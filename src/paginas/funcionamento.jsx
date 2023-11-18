718f1cf46b20061eba992c5f5360abdf59cbfdd0
EmilyMFerreira <marcelineferrera@gmail.com>
2023-11-18 15:03:41 -0300

    nao sei

--- src/paginas/funcionamento.jsx
+++ src/paginas/funcionamento.jsx
@@ -1,42 +1,6 @@
 import { Link } from 'react-router-dom'
 import './css/funcio.css'
 
-import funcionamento from "../imagens/funcionamento1-11.png"
+import { Funcionamento } from './Funcionamento.1'
 
-function Funcionamento() {
-    return (
-    <>
-
-        <header>
-    <h1>Funcionamento</h1>
-    </header>
-
-        <div className="func1">
-        <img src={funcionamento} alt="funcio1" />
-      <p>
-    O Arduino é uma plataforma amplamente utilizada na prototipagem eletrônica graças à sua facilidade de programação e implantação. Hoje faremos uso dessas características para 
-    desenvolver um robô seguidor de linha e entender na prática como essas máquinas amplamente utilizadas na indústria funcionam.
-    </p>
-    </div>
-
-        <div className="func2">
-    <p>
-    E pensando no problema que vários escritórios tem em questão a movimentação de documentos e etc.
-    Pensamos em uma forma de resolver esse problema.
-    </p>
-    </div>
-
-    <div className="func3">
-                <p>
-                    O Guido é um robô móvel guiado por linhas, tendo a função de transportar materiais de forma eficiente em ambientes
-                    onde ele seja útil e possa ajudar na realização de atividades.
-                </p>
-
-            </div>
-
-    
-    </>
-     )
-    }
-    
     export default Funcionamento
