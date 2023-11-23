aad0d46236e9ca28e383f6be6ec5eea1d97b012c
EmilyMFerreira <marcelineferrera@gmail.com>
2023-11-23 10:50:44 -0300

    obj

--- src/paginas/objetivo.jsx
+++ src/paginas/objetivo.jsx
@@ -1,28 +1,29 @@
 import { Link } from 'react-router-dom'
 import './css/objetivo.css'
 
-import objetivo from "../imagens/objetivo.png"
-
 function Objetivo() {
     return (
     <>
 
+
+<br />
+
+
 <header><h1>Objetivo</h1></header>
 
-<div className="imgobj">
-<img src= {objetivo} alt="" />
-</div>
-        
+<br/>
+
 
 <div className="obj">
     <p>
-    O nosso pensamento na criação do guido foi facilitar e deixar mas seguro o transporte desses documentos, que
-    podem ser perdidos e até danificados,oque pode ser prejudicial.
+    O nosso pensamento na criação do Guido, foi pensado principalmente para as empresas que possuem grande movimentação de produtos, oque pode acabar acontecendo é que esses produtos podem ser perdidos ou até danificados. O objetivo do Guido é facilitar esse transporte de uma maneira mais segura e eficiente.
+
     </p>
 
     </div>
 
 
+        <br />
         <br />
 
     <header><h1>Público Alvo</h1></header>
@@ -31,8 +32,7 @@ function Objetivo() {
 
 <div className="conteudop">
    <p>
-    O tipo de cliente que buscamos são escritórios que buscam econimizar tempo transportando documentos.
-    Mas é nosso produto pode se adaptar aos interessados de outras áreas.
+   O nosso publico alvo são empresas que procuram economizar tempo na transferência de arquivos ou materiais de um setor a outro. O Guido pode até ser adaptado para outros tipos de áreas como restaurantes e lojas de roupas, mas por hora ele é apenas um protótipo pequeno.
    </p>
    
    <br />
