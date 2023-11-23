b3d31cc00a783c4ee69ed5e9155ccee47a43b51b
EmilyMFerreira <marcelineferrera@gmail.com>
2023-11-22 22:05:17 -0300

    fonte

--- src/paginas/home.jsx
+++ src/paginas/home.jsx
@@ -20,6 +20,8 @@ function Home() {
          <p>Seja bem-vindo ao emocionante mundo da inovação tecnológica sobre rodas! Apresentamos a você nosso Carrinho controlado por arduino, uma criação que une a magia da robótica com a versatilidade da programação para ajudar no seu ambiente de trabalho.</p>
             </div>
 
+        <br />
+        <br />
             <div className="home2">
                 <p>
                 E pensando nisso projetamos um robô móvel ou um “carrinho”,que terá a função de fazer esse transporte de forma agilizada.
